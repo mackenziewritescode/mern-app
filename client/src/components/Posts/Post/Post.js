def11ada398d0900a4d8b07720aba1abc6b4bfda
mackenziewritescode <mackenzie.charlton@gmail.com>
2021-03-22 17:29:49 -0400

    added date

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,20 @@
 import React from "react";
-// import { formatRelative } from "date-fns";
+// import Dropdown from 'react-dropdown'
+import { parseISO, formatRelative, formatDistance, format } from "date-fns";
 
 import "./styles.scss";
 
+const formatDate = (timestamp) => {
+  timestamp = parseISO(timestamp);
+  const timeAgo = formatDistance(timestamp, new Date(), { addSuffix: true });
+  const date = format(timestamp, "MM/dd/yyyy");
+  return `${timeAgo} on ${date}`;
+};
+
 export const Post = (props) => {
   const imageCheck = props.image ? "post-content" : "post-content no-image";
+
+  const time = formatDate(props.date);
 
   return (
     <div id="post">
@@ -16,7 +26,7 @@ export const Post = (props) => {
       <div className={imageCheck}>
         <h4 className="post-item title">{props.title}</h4>
         <div className="post-item author">
-          Posted by {props.author} two minutes ago.
+          Posted by {props.author} {time}.
         </div>
         <div className="post-item content">{props.content}</div>
         <div className="replies">Replies: {props.replies.length}</div>
