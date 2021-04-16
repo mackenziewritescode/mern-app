76591acf2e84fe2ed75e504b16079addef61447c
mackenziewritescode <mackenzie.charlton@gmail.com>
2021-04-16 16:47:49 -0400

    working on deleteReply

--- server/controllers/replies.js
+++ server/controllers/replies.js
@@ -1,4 +1,4 @@
-// import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 import Reply from "../models/replyModel.js";
 
@@ -7,9 +7,20 @@ export const getReplies = async (req, res) => {
     const replies = await Reply.find();
     res.status(200).json(replies);
   } catch (error) {
-    res.status(400).json({ message: error });
+    res.status(404).json({ message: error });
   }
 };
+
+// export const createReply = async (req, res) => {
+//   const newReply = new Reply(req.body);
+
+//   try {
+//     await newReply.save();
+//     res.status(201).json(newReply);
+//   } catch (error) {
+//     res.status(409).json({ message: error });
+//   }
+// };
 
 export const createReply = async (req, res) => {
   const newReply = new Reply(req.body);
