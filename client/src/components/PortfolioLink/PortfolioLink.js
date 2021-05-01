b474684c49b5c105ad4ca63dd367de1584825ca6
mackenziewritescode <mackenzie.charlton@gmail.com>
2021-05-01 18:49:14 -0400

    adding portfolio link

--- client/src/components/PortfolioLink/PortfolioLink.js
+++ client/src/components/PortfolioLink/PortfolioLink.js
@@ -1,6 +1,7 @@
 import "./styles.scss";
+import { RiArrowGoBackLine } from "react-icons/ri";
 
-export const PortFolioLink = (
+export const PortfolioLink = (
   <a id="drum-portfolio" href="http://www.sunkenworld.com/#page-3">
     <p id="drum-portfolio-text">
       <RiArrowGoBackLine /> Back to portfolio
